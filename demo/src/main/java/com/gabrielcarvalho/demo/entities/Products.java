6e8b69739dfea878041db940b7c00eb34bf5c95a
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-06-14 17:57:31 -0300

    feat: finalizando a API

---
+++ demo/src/main/java/com/gabrielcarvalho/demo/entities/Products.java
@@ -0,0 +1,51 @@
+package com.gabrielcarvalho.demo.entities;
+
+public class Products {
+	private int id;
+	private String name;
+	private Double price;
+	
+	private Departments department;
+	
+	public Products(int id, String name, Double price, Departments department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.department = department;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Departments getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Departments department) {
+		this.department = department;
+	}
+	
+	
+}
